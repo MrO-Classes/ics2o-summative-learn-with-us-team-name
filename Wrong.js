76537c7d51f176a71820dff0fc34c8140491cdbc
ConnorShultz <55148193+ConnorShultz@users.noreply.github.com>
2020-01-26 12:44:08 -0500

    Update Wrong.js
    Final code for Wrong.js

--- Wrong.js
+++ Wrong.js
@@ -11,14 +11,25 @@ function Wrong() {
     if (mouseX > 256 && mouseX < 326 && mouseIsPressed) {
       if (mouseY > 300 && mouseY < 340) {
         drawGameScene();
+        
+        //stop horn && dead
+        mySound3.stop();
+        mySound1.stop();
+        
+        //main theme
+        mySound.setVolume(5);
+        mySound.play();
+      
+        //pipe noise
+        mySound2.setVolume(10);
+        mySound2.play();
       }
     }
 
     background(255, 0, 0)
     textSize(50)
-    text("Wrong!", 175, 100)
-    textSize(30);
-    text("Enemy scored a goal", 115, 200)
+    text("Wrong! You Missed!", 25, 100)
+    text("Enemies scored!", 50, 200)
     drawButton(256, 300, "back")
   }
 }
