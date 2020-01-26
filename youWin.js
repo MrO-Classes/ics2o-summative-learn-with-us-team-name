e5a1cdb4164d9bf30a9f63fe946d912ec1f79693
ConnorShultz <55148193+ConnorShultz@users.noreply.github.com>
2020-01-25 21:10:58 -0500

    Create youWin.js

---
+++ youWin.js
@@ -0,0 +1,10 @@
+function Win() {
+  currentScene = 12
+
+  draw = function() {
+    textSize(50);
+    background(26, 226, 0)
+    text("YOU WIN!", 150, 100)
+    text("You Scored 5 Goals!!", 0, 200)
+  }
+}
