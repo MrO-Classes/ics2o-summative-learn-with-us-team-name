d0e2423dfbe9085e12f6691ddc06aa03fc6d989c
ConnorShultz <55148193+ConnorShultz@users.noreply.github.com>
2020-01-26 12:32:26 -0500

    Update Correct.js

--- Correct.js
+++ Correct.js
@@ -7,12 +7,24 @@ function Correct() {
     if (mouseX > 256 && mouseX < 326 && mouseIsPressed) {
       if (mouseY > 300 && mouseY < 340) {
         drawGameScene();
+        
+        //stopping horn && dead
+        mySound3.stop();
+        mySound1.stop();
+        
+        //main theme
+        mySound.setVolume(5);
+        mySound.play();
+      
+        //pipe noise
+        mySound2.setVolume(10);
+        mySound2.play();
       }
     }
     textSize(50)
     background(26, 226, 0)
-    text("Correct!", 200, 100)
-    text("You Scored A Goal!!", 0, 200)
+    text("Correct! You scored!", 20, 100)
+    text("Enemies missed!", 75, 200)
     drawButton(256, 300, "next")
   }
 }
