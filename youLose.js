c98737f682ca1836fce00ec00587161ace372ef6
ConnorShultz <55148193+ConnorShultz@users.noreply.github.com>
2020-01-26 12:48:43 -0500

    Update youLose.js

--- youLose.js
+++ youLose.js
@@ -1,10 +1,12 @@
 function Lose() {
-  currentScene = 13
-
+  currentScene = 13;
+  activeScreen = "Loose";
   draw = function() {
     textSize(50);
-    background(26, 226, 0)
-    text("you lose!", 150, 100)
-    text("enemy scored 5 goals", 0, 200)
+    background(26, 226, 0);
+    text("YOU LOOSE!", 125, 100);
+    text("Enemy scored 5 goals", 0, 200);
+    textSize(20);
+    text("Press play (top left of screen) to play again", 60, 370);
   }
 }
